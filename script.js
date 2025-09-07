03d232e9ce7983187309f9367fed2e2140637ae4
Philip Boye Addo <boyephilip7@gmail.com>
2025-09-07 15:06:37 -0700

    modified:   admin-dashboard.html 	modified:   admin-login-page.html 	modified:   edit-locations.html 	modified:   edit-paths.html 	modified:   index.html 	new file:   index2.html 	new file:   landmarks.json 	modified:   main.js 	new file:   script.js 	modified:   style.css 	new file:   style2.css
        switched to using leaflet.js

--- script.js
+++ script.js
@@ -1,4 +1,262 @@
-//switches to the page with the passed name
+let landmarks = {};
+
+fetch('landmarks.json')
+  .then(res => res.json())
+  .then(data => {
+    landmarks = data;
+    Object.keys(landmarks).forEach(name => {
+      L.marker(landmarks[name])
+        .addTo(map)
+        .bindPopup(`<b>${name.toUpperCase()}</b>`);
+    });
+  });
+
+const map = L.map('map').setView([5.596, -0.223], 17);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 20,
+}).addTo(map);
+
+function searchLocation() {
+  const input = document.getElementById("searchInput").value.toLowerCase();
+  const coords = landmarks[input];
+
+  if (coords) {
+    const msg = new SpeechSynthesisUtterance(`Navigating to ${input}`);
+    window.speechSynthesis.speak(msg);
+
+    map.setView(coords, 18);
+    L.popup()
+      .setLatLng(coords)
+      .setContent(`<b>${input.toUpperCase()}</b>`)
+      .openOn(map);
+  } else {
+    alert("Location not found. Try another name.");
+  }
+}
+
+function toggleContrast() {
+  document.body.classList.toggle("high-contrast");
+}
+
+function increaseTextSize() {
+  document.body.classList.toggle("large-font");
+}
+
+// Example accessible path
+const accessiblePath = [
+  [5.6181, -0.2204],
+  [5.6178, -0.2198]
+];
+
+L.polyline(accessiblePath, {
+  color: 'blue',
+  weight: 5,
+  dashArray: '10, 10'
+}).addTo(map).bindPopup("Accessible Path");
+
+let routeMode = null;
+let startPoint = null;
+let endPoint = null;
+let routeLine = null;
+
+function setRouteMode(mode) {
+  routeMode = mode;
+  alert(`Click on the map to set the ${mode} point`);
+}
+
+map.on('click', function (e) {
+  if (!routeMode) return;
+
+  if (routeMode === 'start') {
+    if (startPoint) map.removeLayer(startPoint);
+    startPoint = L.marker(e.latlng, { draggable: true }).addTo(map).bindPopup("Start Point").openPopup();
+  }
+
+  if (routeMode === 'end') {
+    if (endPoint) map.removeLayer(endPoint);
+    endPoint = L.marker(e.latlng, { draggable: true }).addTo(map).bindPopup("End Point").openPopup();
+  }
+
+  routeMode = null;
+});
+
+function drawRoute() {
+  if (startPoint && endPoint) {
+    const startLatLng = startPoint.getLatLng();
+    const endLatLng = endPoint.getLatLng();
+
+    if (routeLine) map.removeLayer(routeLine);
+
+    routeLine = L.polyline([startLatLng, endLatLng], {
+      color: 'green',
+      weight: 4
+    }).addTo(map).bindPopup("Custom Route").openPopup();
+  } else {
+    alert("Please select both Start and End points.");
+  }
+}
+
+function clearRoute(){
+  startPoint.remove();
+  endPoint.remove();
+  routeLine.remove();
+}
+
+//gets the user's location
+function getUser(){
+  map.locate({setView: true, maxZoom: 16});
+  //when location is found
+  function onLocationFound(e){
+    var radius = e.accuracy / 2;
+    //add a marker
+    L.marker(e.latlng).addTo(map).bindPopup(`You are within ${radius} meters from this point`).openPopup();
+    
+    //add accuracy circle
+    L.circle(e.latlng, radius).addTo(map);
+  }
+
+  //when location is not found
+  function onLocationError(e){
+    alert(e.message);
+  }
+
+  map.on('locationfound', onLocationFound);
+  map.on('locationerror', onLocationError);
+}
+
+// map.on('click', function(e){
+//   console.log(`lat: ${e.latlng.lat}, long: ${e.latlng.lng}`);
+// });
+
+//marker for Main Building
+let mainBuildingMarker = L.marker([5.596841187565221, -0.2232778072357178]);
+  mainBuildingMarker.addTo(map);
+mainBuildingMarker.on('click', function(){
+  showLocMenu2(99, 113);
+});
+
+//marker for Block B
+let blockBMarker = L.marker([5.596520932579942, -0.22373378276824954]);
+  blockBMarker.addTo(map);
+blockBMarker.on('click', function(){
+  showLocMenu2(51, 213);
+});
+
+//marker for Cafeteria
+let cafeteriaMarker = L.marker([5.596035065040515, -0.2237096428871155]);
+  cafeteriaMarker.addTo(map);
+cafeteriaMarker.on('click', function(){
+  showLocMenu2(46, 333);
+});
+
+//marker for Engineering Block
+let engineeringBlockMarker = L.marker([5.5961016396499375, -0.22299617528915408]);
+  engineeringBlockMarker.addTo(map);
+engineeringBlockMarker.on('click', function(){
+  showLocMenu2(190, 296);
+});
+
+//marker for Hostel Block A
+let hostelBlockAMarker = L.marker([5.595703964235648, -0.2236077189445496]);
+  hostelBlockAMarker.addTo(map);
+hostelBlockAMarker.on('click', function(){
+  showLocMenu2(45, 392);
+});
+
+//marker for Hostel Block B
+let hostelBlockBMarker = L.marker([5.595463928846843, -0.22325634956359866]);
+  hostelBlockBMarker.addTo(map);
+hostelBlockBMarker.on('click', function(){
+  showLocMenu2(136, 453);
+});
+
+//marker for Hostel Block C
+let hostelBlockCMarker = L.marker([5.5955816621938155, -0.22296130657196048]);
+  hostelBlockCMarker.addTo(map);
+hostelBlockCMarker.on('click', function(){
+  showLocMenu2(190, 419);
+});
+
+//marker for Airport
+let airportMarker = L.marker([5.59581909725854, -0.22292912006378174]);
+  airportMarker.addTo(map);
+airportMarker.on('click', function(){
+  showLocMenu2(231, 373);
+});
+
+//marker for Graduate School
+let graduateSchoolMarker = L.marker([5.595919965214853, -0.22272393107414248]);
+  graduateSchoolMarker.addTo(map);
+graduateSchoolMarker.on('click', function(){
+  showLocMenu2(264, 352);
+});
+
+//marker for Great Hall
+let greatHallMarker = L.marker([5.59552280973244, -0.223897397518158]);
+  greatHallMarker.addTo(map);
+greatHallMarker.on('click', function(){
+  showLocMenu2(7, 454);
+});
+
+//marker for Bush Canteen
+let bushCanteenMarker = L.marker([5.595234371202593, -0.22369086742401126]);
+  bushCanteenMarker.addTo(map);
+bushCanteenMarker.on('click', function(){
+  showLocMenu2(48, 494);
+});
+
+//marker for Football Field
+let footballFieldMarker = L.marker([5.59502370788417, -0.2233877778053284]);
+  footballFieldMarker.addTo(map);
+footballFieldMarker.on('click', function(){
+  showLocMenu2(129, 570);
+});
+
+function clearAllLocationMakers(){
+  searchingLocation = true;
+  mainBuildingMarker.remove();
+  blockBMarker.remove();
+  cafeteriaMarker.remove();
+  engineeringBlockMarker.remove();
+  hostelBlockAMarker.remove();
+  hostelBlockBMarker.remove();
+  hostelBlockCMarker.remove();
+  airportMarker.remove();
+  graduateSchoolMarker.remove();
+  greatHallMarker.remove();
+  bushCanteenMarker.remove();
+  footballFieldMarker.remove();
+}
+
+function addAllMarkers(){
+  searchingLocation = true;
+  mainBuildingMarker.addTo(map);
+  blockBMarker.addTo(map);
+  cafeteriaMarker.addTo(map);
+  engineeringBlockMarker.addTo(map);
+  hostelBlockAMarker.addTo(map);
+  hostelBlockBMarker.addTo(map);
+  hostelBlockCMarker.addTo(map);
+  airportMarker.addTo(map);
+  graduateSchoolMarker.addTo(map);
+  greatHallMarker.addTo(map);
+  bushCanteenMarker.addTo(map);
+  footballFieldMarker.addTo(map);
+}
+
+function checkSearchInput(){
+  var location = document
+      .getElementById("searchInput")
+      .value.trim();
+  if(location == ""){
+    addAllMarkers();
+  }
+}
+
+//----------------------------------------
+//-- JS Code for other site starts here --
+//----------------------------------------
+  //switches to the page with the passed name
 function goToPage(pageName) {
   window.location.href = pageName;
 }
@@ -662,8 +920,90 @@ function loadBuildingDataArray() {
 }
 
   
-function showMessage(){
-  alert("Message from other JS file!");
+//indicates the chosen location on the map
+function showLocation(){
+  var location = document
+      .getElementById("searchInput")
+      .value.trim();
+  clearAllLocationMakers();
+
+    if (
+      location == "Main Building" ||
+      location == "Executive Wing" ||
+      location == "Head, Human Resource" ||
+      location == "A.G Director, Works & Physical Development" ||
+      location == "Main Building Washrooms - Top" ||
+      location == "Dep. Director of Finance" ||
+      location == "Internal Audit" ||
+      location == "HR. Department" ||
+      location == "Information Technology Support Services Office" ||
+      location == "Library" ||
+      location == "Information Center" ||
+      location == "Dr. Eva Von Hirsch Auditorium" ||
+      location == "GCTU Bookshop" ||
+      location == "Admissions Office" ||
+      location == "Accounts Reconciliation" ||
+      location == "Office of the Dean of Student Affairs" ||
+      location == "President's Conference Room" ||
+      location == "Accounts Receivable/ Accounts Office" ||
+      location == "Video Conference Center" ||
+      location == "Main Building Washrooms - Ground" ||
+      location == "Graduate Resource Center"
+    ) {
+      mainBuildingMarker.addTo(map);
+    } else if (
+      location == "Block B" ||
+      location == "Lectures Resource Center" ||
+      location == "Academic Affairs Directorate" ||
+      location == "Switching Lab" ||
+      location == "Block B Washrooms - Ground" ||
+      location == "CPD Office" ||
+      location == "Computer Lab" ||
+      location == "Center for Online Learning and Teaching (COLT)" ||
+      location == "Software Systems Unit" ||
+      location == "Security Office" ||
+      location == "B1" ||
+      location == "B2" ||
+      location == "Block B Washrooms - Top" ||
+      location == "Final Year Project Room" ||
+      location == "B3" ||
+      location == "B4"
+    ) {
+      blockBMarker.addTo(map);
+    } else if (location == "Cafeteria" || location == "GCTU Radio") {
+      cafeteriaMarker.addTo(map);
+    } else if (location == "Hostel Block A") {
+      hostelBlockAMarker.addTo(map);
+    } else if (
+      location == "Engineering Block" ||
+      location == "HOD - Computer Engineering" ||
+      location == "HOD - Electrical/ Electronics Engineering" ||
+      location == "HOD - Telecom Engineering" ||
+      location == "Office of the Dean Faculty of Engineering" ||
+      location == "Electronics Lab" ||
+      location == "Energy Systems Lab" ||
+      location == "Reprographic Center" ||
+      location == "Engineering Conference Center"
+    ) {
+      engineeringBlockMarker.addTo(map);
+    } else if (location == "Hostel Block B") {
+      hostelBlockBMarker.addTo(map);
+    } else if (location == "Hostel Block C") {
+      hostelBlockCMarker.addTo(map);
+    } else if (location == "Airport") {
+      airportMarker.addTo(map);
+    } else if (location == "Graduate School") {
+      graduateSchoolMarker.addTo(map);
+    } else if (location == "GCTU Great Hall") {
+      greatHallMarker.addTo(map);
+    } else if (location == "Bush Canteen") {
+      bushCanteenMarker.addTo(map);
+    } else if (location == "Football Field") {
+      footballFieldMarker.addTo(map);
+    } else if (location == "Get current location") {
+      // (startX = userX), (startY = userY);
+      //userX = startX, userY = startY;
+    }  
 }
 
 // function isValidJSON(str){
